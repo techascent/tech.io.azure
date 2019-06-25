f882a1652fd8f71ff415964d7386e73138bc4106
Chris Nuernberger <chris@techascent.com>
2019-06-25 13:20:16 -0600

    got metadata and such.  Think I am on my way.

--- src/tech/io/azure/blob.clj
+++ src/tech/io/azure/blob.clj
@@ -1,4 +1,25 @@
 (ns tech.io.azure.blob
-  (:import [com.microsoft.azure.storage.blob CloudBlobClient]
-           [com.microsoft.azure.storage StorageUri StorageCredentials]
+  (:import [com.microsoft.azure.storage.blob CloudBlobClient CloudBlobContainer]
+           [com.microsoft.azure.storage
+            StorageUri StorageCredentials StorageCredentialsAccountAndKey
+            CloudStorageAccount]
            [java.net URI]))
+
+(defn blob-client
+  ^CloudBlobClient [^String account-name ^String account-key]
+  (-> (StorageCredentialsAccountAndKey. account-name account-key)
+      ;;true means to use https
+      (CloudStorageAccount. true)
+      (.createCloudBlobClient)))
+
+
+(comment
+  (require '[tech.io.azure.auth :as azure-auth])
+  (require '[tech.config.core :as config])
+  (def creds (azure-auth/vault-azure-blob-creds
+              (config/get-config :tech-azure-blob-vault-path) {}))
+  (def account-key (:tech.azure.blob/account-key creds))
+  (def account-name (:tech.azure.blob/account-name creds))
+  (def client (blob-client account-name account-key))
+  (def containers (vec (.listContainers client)))
+  )
