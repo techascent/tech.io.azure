9d6ca6db87ab02c92dcf198da98a74a2663e88fd
Chris Nuernberger <chris@techascent.com>
2020-01-22 15:00:58 -0700

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.io.azure "3.10"
+(defproject techascent/tech.io.azure "3.10-1-SNAPSHOT"
   :description "Bindings to at least blob storage for tech.io system."
   :url "http://github.com/techascent/tech.io.azure"
   :license {:name "EPL-2.0"
