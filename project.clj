4bcdd3fa3aef70273d6ef2149166f604805b587f
Chris Nuernberger <chris@techascent.com>
2020-01-23 10:39:01 -0700

    3.12

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject techascent/tech.io.azure "3.11-1-SNAPSHOT"
+(defproject techascent/tech.io.azure "3.12"
   :description "Bindings to at least blob storage for tech.io system."
   :url "http://github.com/techascent/tech.io.azure"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [com.microsoft.azure/azure-storage "8.3.0"]
-                 [techascent/tech.io "3.11"]]
+                 [techascent/tech.io "3.12"]]
   :profiles {:dev {:dependencies [[amperity/vault-clj "0.7.0"]
                                   [ch.qos.logback/logback-classic "1.1.3"]]}})
